bed7316d8f99b358bbca3bd46c773b60ba460a13
Lorenzo Belletti <lorenzo.belletti5@unibo.studio.it>
2025-11-30 12:39:34 +0100

    Added the DrawNumberView method in class and interface

--- 102-advanced-mvc/src/main/java/it/unibo/mvc/DrawNumberView.java
+++ 102-advanced-mvc/src/main/java/it/unibo/mvc/DrawNumberView.java
@@ -24,4 +24,9 @@ public interface DrawNumberView {
      * @param res the result of the last draw
      */
     void result(DrawResult res);
+    
+    /**
+     * @param message the error to show
+     */
+    void displayError(String message);
 }
